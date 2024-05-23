893f7acee65a6970b6b5e6721102b22192090129
Mario <polmy015@mymail.unisa.edu.au>
2024-05-23 20:09:10 +0930

    Duplicate Commands for Alarm Section

--- src/06.ui/component/ButtonStop.js
+++ src/06.ui/component/ButtonStop.js
@@ -2,12 +2,12 @@ import { Stop } from '../../01.entities/stop-button/Stop.js';
 import { PressAndHoldButton } from '../../99.utils/ui/PressAndHoldButton.js'; 
 import { CircleButtonUI } from '../js/CircleButtonUI.js';
 
-export function StopButton(){
+export function StopButton(elementN){
     const id = 3; // Id use for config DataWriteList.js
     const elementId = 'buttonStop'; // Id of button 
     const entity = new Stop();
     const elementUI = new CircleButtonUI(elementId, 'btn_circle_out_STOP')
 
-    const button = new PressAndHoldButton(id, elementId, entity, elementUI );
+    const button = new PressAndHoldButton(id, elementId,elementN, entity, elementUI );
     return button;
 }
---
+++ src/99.utils/ui/PressAndHoldButtonByClass.js
@@ -0,0 +1,65 @@
+import { UpdateBooleanValueUseCase } from '../../02.usecases/entities/UpdateBooleanValueUseCase.js';
+import { UpdateValueRepository } from '../../03.repositories/entities/UpdateValueRepository.js';
+import { plcCommunicationManager } from '../../02.usecases/communication/PLCcommunication.js';
+
+export class PressAndHoldButtonByClass {
+    constructor(id,elementId,elementN, entity, elementUI, validateFn = () => true,timeToHold = 0) {
+        this.element = document.getElementsByClassName(elementId)[elementN];
+        this.elementUI = elementUI;
+        this.pressed = false;
+        this.id = id;
+        this.entity = entity;
+        this.validateFn = validateFn;
+        this.timeToHold = timeToHold;
+
+        const repository = new UpdateValueRepository(plcCommunicationManager);
+        this.usecase = new UpdateBooleanValueUseCase(repository, this.entity);
+
+        this.handleMouseDown = this.handleMouseDown.bind(this);
+        this.handleMouseUp = this.handleMouseUp.bind(this);
+        this.handleMouseLeave = this.handleMouseLeave.bind(this);
+
+        this.element.addEventListener('mousedown', this.handleMouseDown);
+        this.element.addEventListener('touchstart', this.handleMouseDown);
+        this.element.addEventListener('mouseup', this.handleMouseUp);    
+        this.element.addEventListener('touchend', this.handleMouseUp);
+        this.element.addEventListener('mouseleave', this.handleMouseLeave);
+    }
+
+    handleMouseDown(event) {
+        event.preventDefault();
+
+        // Validate before proceeding
+        if (!this.validateFn()) {
+            console.error('Validation failed');
+            return;
+        }
+        
+        this.setValue(true);
+        this.elementUI.showPressed();
+    }
+
+    handleMouseUp(event) {
+        event.preventDefault();
+        this.setValue(false);
+        this.elementUI.showUnpressed();
+    }
+
+    handleMouseLeave(event) {
+        if (this.pressed) {
+            this.setValue(false);
+            this.elementUI.showUnpressed();
+        }
+    }
+
+    setValue(value) {
+        try {
+            setTimeout(() => {
+                this.usecase.update(this.id, value);
+                this.pressed = value;
+            }, this.timeToHold);
+        } catch (error) {
+            console.error('Error setting:', error);
+        }
+    }
+}
